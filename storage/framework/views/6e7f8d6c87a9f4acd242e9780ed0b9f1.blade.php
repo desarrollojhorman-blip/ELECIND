498ca8f0bd63563008d779d80ad6e5b6d22689cd
Jhorman <desarrollo@entreredes.es>
2026-05-18 12:45:15 +0200

    ajustes ok_

--- storage/framework/views/6e7f8d6c87a9f4acd242e9780ed0b9f1.blade.php
+++
@@ -1,3 +0,0 @@
-<svg x-show="open || drawerOpen" x-bind:class="(isExpanded('configuracion') || true) ? 'rotate-90' : ''" class="size-4 shrink-0 text-slate-400 transition-transform" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-  <path fill-rule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd"/>
-</svg>
--- storage/framework/views/fa827a7d46b25dd02c3553b4114becee.blade.php
+++
@@ -1,3 +0,0 @@
-<svg x-show="open || drawerOpen" x-bind:class="menuOpen ? '' : 'rotate-180'" class="size-4 shrink-0 text-slate-400 transition-transform" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-  <path fill-rule="evenodd" d="M9.47 6.47a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 1 1-1.06 1.06L10 8.06l-3.72 3.72a.75.75 0 0 1-1.06-1.06l4.25-4.25Z" clip-rule="evenodd"/>
-</svg>
