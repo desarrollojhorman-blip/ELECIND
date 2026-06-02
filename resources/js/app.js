0c0ed81f5f819f2546ef4afe319d8e9ba3059181
Jhorman <desarrollo@entreredes.es>
2026-06-02 13:49:23 +0200

    v2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,13 @@
 import './bootstrap';
 import Alpine from 'alpinejs';
+import persist from '@alpinejs/persist';
 
 import flatpickr from 'flatpickr';
 import { Spanish } from 'flatpickr/dist/l10n/es.js';
 import 'flatpickr/dist/flatpickr.min.css';
 
 window.Alpine = Alpine;
+Alpine.plugin(persist);
 Alpine.start();
 
 // Locale español por defecto en todos los flatpickr de la app.
